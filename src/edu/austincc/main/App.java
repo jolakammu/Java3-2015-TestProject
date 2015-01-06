c31618e79a87450c9a797dc978b45b1ce9d91589
jolakammu <jolakammu@yahoo.com>
2015-01-06 12:06:06 -0600

    initial commit

---
+++ src/edu/austincc/main/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.austincc.main;
+
+/**
+ * @author jolak_000
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
